4a7310ef28646b6b0c6f62dde914c26a8b696afc
Thar Nyi Phyo <konyi.tharnyiphyo@gmail.com>
2024-12-31 17:02:31 +0630

    Upade Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my_portfolio/',
+  base: '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
